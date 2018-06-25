1484434dc3064dd05b2f02dc1967ce8611102dd1
abadug5793 <bavani7777@gmail.com>
2018-06-25 14:04:51 +0000

    automatically backed up by learn

--- cars.rb
+++ cars.rb
@@ -1,27 +1,11 @@
 class Cars
-  attr_accessor  :stereo, :cam                #attribute accessor
+  attr_accessor  :stereo, :cam               #attribute accessor
 
   def initialize(model, make, year)
     @model = model
     @make = make
     @year = year
     puts "I love my #{year} #{model}. It is a #{make}"
-  end
-
-  def stereo=(car_stereo)          #setting
-    @stereo = car_stereo
-  end
-
-  def stereo
-    @stereo
-  end
-
-  def cam=(backup_cam)
-    @cam = backup_cam
-  end
-
-  def cam
-    @cam
   end
 
 end
