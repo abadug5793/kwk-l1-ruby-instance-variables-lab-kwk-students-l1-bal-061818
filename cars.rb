7a70a42a8d1b3472e5152fc473d480c90889b7c9
abadug5793 <bavani7777@gmail.com>
2018-06-25 13:58:42 +0000

    automatically backed up by learn

--- cars.rb
+++ cars.rb
@@ -6,7 +6,18 @@ class Cars
     @year = year
     puts "I love my #{year} #{model}. It is a #{make}"
   end
+
+  def stereo=(car_stereo)
+    @stereo = car_stereo
+  end
+
+  def cam=(backup_cam)
+    @cam = backup_cam
+  end
+  
 end
 
 toyota = Cars.new("Camry", "Toyota", "1997")
 prius = Cars.new("Prius", "Toyota", "2011")
+
+
